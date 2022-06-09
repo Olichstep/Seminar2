b9b2e91a83d0494caf7aaf4d60573cf97835c740
Olga Stepanova <olichstep@gmail.com>
2022-06-09 10:29:09 +0300

    1 задача 2 семинара

---
+++ Program_1.cs
@@ -0,0 +1,17 @@
+﻿// 
+
+int num = new Random().Next(10, 100);
+
+Console.WriteLine(num);
+
+int c1 = num / 10;  // 56 / 10 = 5 (первая цифра слева)
+int c2 = num % 10;  // (остаток, вторая цифра)
+
+if (c1 < c2)
+{
+    Console.WriteLine(c2);
+}
+else 
+{
+    Console.WriteLine(c1);
+}
